dd71813153fcd6a3999e424950be46589f4adae7
Camper_Samu <samueleverga@gmail.com>
2024-01-23 01:02:18 +0100

    new: CommanderSource to ServerCommandSource util method

--- src/main/java/com/campersamu/itemcommander/nbt/CommanderSource.java
+++ src/main/java/com/campersamu/itemcommander/nbt/CommanderSource.java
@@ -1,5 +1,16 @@
 package com.campersamu.itemcommander.nbt;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import static java.util.Objects.requireNonNull;
+
 public enum CommanderSource {
     DANGEROUSLY_OP((byte) -2),
     SERVER_AS_PLAYER((byte) -1),
@@ -21,5 +32,25 @@ public enum CommanderSource {
             case 2 -> OP;
             default -> SERVER;
         };
+    }
+
+    @Environment(EnvType.SERVER)
+    @SuppressWarnings("deprecation")
+    public static ServerCommandSource vanilla(@NotNull CommanderSource source, @Nullable ServerPlayerEntity player) {
+        var server = (player == null)
+                ? ((MinecraftServer) FabricLoader.getInstance().getGameInstance())
+                : player.getServer();
+        assert server != null;  // we're in a serverside env, if not we *need* to crash.
+        return switch (source) {
+            case PLAYER -> requireNonNull(player).getCommandSource();
+            case OP, DANGEROUSLY_OP -> requireNonNull(player).getCommandSource().withLevel(server.getOpPermissionLevel());
+            case SERVER_AS_PLAYER -> server.getCommandSource().withEntity(player);
+            default -> server.getCommandSource();
+        };
+    }
+
+    @Environment(EnvType.SERVER)
+    public ServerCommandSource vanilla(@Nullable ServerPlayerEntity player) {
+        return vanilla(this, player);
     }
 }
